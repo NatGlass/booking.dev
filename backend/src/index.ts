2a3955bcfad509e02d76dbd6d920c1bf1605b203
Visibyte <nathanglass26@gmail.com>
2024-01-18 13:36:47 +0000

    serve frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,15 +2,16 @@ import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
+import path from "path";
 
 mongoose.connect(process.env.DATABASE_URL as string);
-
-
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
