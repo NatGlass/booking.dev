f42db105addb4446f99d9cde351600d543e43837
Visibyte <nathanglass26@gmail.com>
2024-01-18 19:35:33 +0000

    header

--- frontend/src/components/global/Header.tsx
+++ frontend/src/components/global/Header.tsx
@@ -1,9 +1,30 @@
+import { Link } from "react-router-dom";
+import { Button } from "../ui/button";
+
 function Header() {
-    return <div className="bg-rose-100 py-6">
-        <div className="container mx-auto flex justify-between">
-            <span className="text-2xl text-rose-500 font-semibold">Booking.dev</span>
+  return (
+    <div className="bg-rose-100 py-6">
+      <div className="container mx-auto flex justify-between items-center">
+        <span className="text-2xl text-rose-500 font-semibold tracking-wide">
+          <Link to="/">Booking.dev</Link>
+        </span>
+        <div className="flex space-x-2">
+          <Link to="/sign-up">
+            <Button data-testid="sign-up-header-button">Sign Up</Button>
+          </Link>
+          <Link to="/sign-in">
+            <Button
+              variant="secondary"
+              className="text-rose-600"
+              data-testid="sign-in-header-button"
+            >
+              Sign In
+            </Button>
+          </Link>
+        </div>
       </div>
-  </div>;
+    </div>
+  );
 }
 
 export default Header;
