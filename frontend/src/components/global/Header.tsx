77e9808a47b6bf40a992afadb40d6ca0ccf45829
Visibyte <nathanglass26@gmail.com>
2024-01-19 09:33:55 +0000

    increase logo size

--- frontend/src/components/global/Header.tsx
+++ frontend/src/components/global/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className="bg-rose-100 py-6">
       <div className="container mx-auto flex justify-between items-center">
-        <span className="text-2xl text-rose-500 font-semibold tracking-wide">
+        <span className="text-3xl text-rose-500 font-semibold tracking-wide">
           <Link to="/">Booking.dev</Link>
         </span>
         <div className="flex space-x-2">
