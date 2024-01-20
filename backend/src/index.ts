b0d923353a75ec32c490fd8d099bd95f7f566eb2
Visibyte <nathanglass26@gmail.com>
2024-01-20 10:36:58 +0000

    add empty string to base url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,10 +22,11 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoutes);
+
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,6 +1,6 @@
 import { loginFormType, registerFormType } from "./schemas";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
 export const register = async (formData: registerFormType) => {
   const response = await fetch(`${API_BASE_URL}/api/users/register`, {
